ce0c9f8960392acdceb4cc59eef2cce9ac48643c
Zy'on Reynolds <reynoldszyon@gmail.com>
2026-09-16 06:18:40 +0000

    I have added a area for the user to input their name.

--- main.py
+++ main.py
@@ -3,8 +3,14 @@
 # A quiz/questionnaire program built for CS 104 Project 1
 
 # TODO: Define your variables here.
-
+name = input ("What is your name?")
+score = 0
+total_questions = 5
 # TODO: Print a welcome message introducing your program.
+print()
+print("Welcome to the Knowledge-Based Quiz on Music," + name + "!")
+print("Answer each question by entering the number of your choice.")
+print()
 
 # TODO: Write your questions and conditional logic here.
 # Follow the outline you planned in your README.
